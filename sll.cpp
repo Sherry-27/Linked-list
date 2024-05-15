1c849b79a71efecf9a846523cf220ff09021ac31
Sheery <sk9109182@gmail.com>
2024-05-15 15:31:49 +0500

    Update sll.cpp

--- sll.cpp
+++ sll.cpp
@@ -1,117 +1,152 @@
- #include<iostream>
- using namespace std;
- 
- class Node {
- 	public:
- 	int data;
- 	Node* next;
- 	Node (int data) {
- 		this->data = data;
- 		this->next = NULL;
-	 }
- 
-    ~Node (){
-    	int value = this -> data;
-    	if(this->next!=NULL){
-    		delete next;
-    		this->next = NULL;
-		}
-		cout<<"memory is free for node with data: "<<value<<endl;
-	}
-	
+#include <iostream>
+#include <cstdlib> 
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
 };
- void InsertAtHead(Node* &head, int d) {
- 	Node* temp = new Node(d);
- 	temp-> next = head;
- 	head = temp;
- 	
- }
- 
-void InsertAtTail(Node* &tail, int d) {
- 	Node* temp = new Node(d);
- 	tail-> next = temp;
- 	tail = temp;
+
+void insert(Node*& head, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
 }
- void print(Node* head) {
- 	Node* temp = head;
- 	
- 	while(temp!=NULL){
- 		cout<<temp->data<<" ";
- 		temp = temp-> next;
-	 }
-	 cout<< endl;
- }
- 
- void InsertAtPosition(Node* &tail, Node* &head, int position, int d){
- 	if(position==1){
- 		InsertAtHead(head,d);
- 		return;
-	 }
- 	Node* temp = head;
- 	int cnt = 1;
- 	while(cnt<position-1){
- 		temp = temp->next;
- 		cnt++; 
-	 }
-	 if(temp->next==NULL){
-	 	InsertAtTail(tail, d);
-	 	return;
-	 }
-	 Node* nodeToInsert = new Node(d);
-	 nodeToInsert-> next = temp-> next;
-	 temp-> next = nodeToInsert;
- }
- 
- void deleteNode(int position, Node* &head){
- 	if(position == 1){
- 		Node* temp = head; 
- 		head = head -> next;
- 		temp->next = NULL;
- 		delete temp;	
-	 }
-	 else
-	 {
-	 	Node* curr = head;
-	 	Node* prev = NULL;
-	 	int cnt =1;
-	 	while(cnt<position) {
-	 		prev = curr;
-	 		curr = curr->next;
-	 		cnt++;
-		 }
-		 
-		 prev -> next = curr -> next;
-		 curr -> next = NULL;
-		 delete curr;
-	 }
- }
- 
- int main() {
- 	Node* node1 = new Node(10);
- //	cout << node1 -> data << endl;
- //	cout << node1 -> next << endl;
- 	
- 	Node* head = node1;
- 	Node* tail = node1;
- 	print(head);
- 	
- 	InsertAtTail(tail, 12);
- 	
- 	print(head);
- 	
- 	InsertAtTail(tail, 15);
- 	
- 	print(head);
- 	
- 	InsertAtPosition(tail, head, 4, 22);
- 	
- 	print(head);
- 	
- 	cout<< "head: "<<head->data<<endl;
- 	cout<< "tail: "<<tail->data<<endl;
- 	
- 	deleteNode(3, head);
- 	print(head);
- 	
- 	return 0;
- }
+
+void display(Node* head) {
+    if (head == NULL){
+        cout << "Currently empty!";
+    }
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void deleteList(Node** head) {
+    Node* current = *head;
+    Node* next;
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+    *head = NULL;
+}
+
+void deleteSingleNode(Node** head, int key) {
+    Node* temp = *head;
+    Node* prev = NULL;
+
+    if (temp != NULL && temp->data == key) {
+        *head = temp->next;
+        free(temp);
+        cout << "Node with value " << key << " deleted" << endl;
+        return;
+    }
+
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        cout << "Key not found in the linked list. Nothing deleted!" << endl;
+        return;
+    }
+
+    prev->next = temp->next;
+    free(temp);
+    
+    cout << "Node with value " << key << " deleted" << endl;
+}
+
+bool search(Node* head, int key) {
+    Node* temp = head;
+    while (temp != NULL) {
+        if (temp->data == key) {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+int main() {
+    Node* head = NULL;
+    char choice;
+
+    while (true) {
+        cout << "1. Insert a value" << endl;
+        cout << "2. Display the linked list" << endl;
+        cout << "3. Delete the entire linked list" << endl;
+        cout << "4. Delete a node" << endl;
+        cout << "5. Search for node" << endl;
+        cout << "6. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case '1': {
+                int value;
+                cout << "Enter a value to insert into the list: ";
+                cin >> value;
+                insert(head, value);
+                break;
+            }
+            case '2': {
+                cout << "Linked List: ";
+                display(head);
+                break;
+            }
+            case '3': {
+                deleteList(&head);
+                cout << "Linked list deleted" << endl;
+                break;
+            }
+            case '4': {
+                int key;
+                cout << "Enter the value to delete: ";
+                cin >> key;
+                deleteSingleNode(&head, key);
+                break;
+            }
+               case '5': {
+                int key;
+                cout << "Enter the value to search for: ";
+                cin >> key;
+                if (search(head, key)) {
+                    cout << "Value " << key << " found in the linked list" << endl;
+                } else {
+                    cout << "Value " << key << " not found in the linked list" << endl;
+                }
+                break;
+            }
+            case '6': {
+                cout << "Exiting..." << endl;
+                return 0;
+            }
+            default: {
+                cout << "Invalid choice. Please enter again." << endl;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
