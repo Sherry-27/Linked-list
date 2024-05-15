befca51b426760d8924fa6c48529158e0cf0db9c
Sheery <sk9109182@gmail.com>
2024-05-15 15:30:59 +0500

    Update dll.cpp

--- dll.cpp
+++ dll.cpp
@@ -1,153 +1,205 @@
-#include<iostream>
- using namespace std;
- 
- class Node {
- 	public:
- 	int data;
- 	Node* next;
- 	Node* prev;
- 	Node (int d) {
- 		this->data = d;
- 		this->prev = NULL;
- 		this->next = NULL;
-	 }
-	 
-	~Node (){
-    	int value = this -> data;
-    	if(this->next!=NULL){
-    		delete next;
-    		this->next = NULL;
-		}
-		cout<<"memory is free for node with data: "<<value<<endl;
-	}
-	
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
+
+struct studentRecord
+{
+    int rollnumber;
+    float gpa;
+    studentRecord* next;
+    studentRecord* prev;
 };
 
- void InsertAtHead(Node* &head, int d) {
- 	Node* temp = new Node(d);
- 	temp-> next = head;
- 	head->prev = temp;
- 	head = temp;
- 	
- }
- 
- void InsertAtTail(Node* &tail, int d) {
- 	Node* temp = new Node(d);
- 	tail-> next = temp;
- 	temp->prev = tail;
- 	tail = temp;
+bool insert(studentRecord** head)
+{
+    studentRecord* ptr = (studentRecord*)malloc(sizeof(studentRecord));
+    cout << "Insert Roll Number: ";
+    cin >> ptr->rollnumber;
+    cout << "Insert GPA: ";
+    cin >> ptr->gpa;
+    ptr->next = NULL;
+    ptr->prev = NULL;
+    if (*head == NULL)
+    {
+        *head = ptr;
+        return true;
+    }
+    if (ptr->rollnumber < (*head)->rollnumber)
+    {
+        (*head)->prev = ptr;
+        ptr->next = *head;
+        *head = ptr;
+        return true;
+    }
+    if (ptr->rollnumber > (*head)->rollnumber)
+    {
+        studentRecord* curr = *head;
+        studentRecord* cnext = curr->next;
+        while (cnext != NULL)
+        {
+            if (ptr->rollnumber < cnext->rollnumber)
+            {
+                curr->next = ptr;
+                cnext->prev = ptr;
+                ptr->prev = curr;
+                ptr->next = cnext;
+                return true;
+            }
+            else
+            {
+                curr = cnext;
+                cnext = cnext->next;
+            }
+        }
+        curr->next = ptr;
+        ptr->prev = curr;
+        return false;
+    }
 }
 
-void print(Node* head){
-	Node* temp = head;
-	
-	while(temp != NULL){
-		cout<< temp->data<<" ";
-		temp = temp->next;
-	}
-	cout<<endl;
+void search(studentRecord** head)
+{
+    int toSearch;
+    cout << "Enter student's rollnumber: ";
+    cin >> toSearch;
+
+
+    studentRecord* curr = *head;
+    while (curr != NULL)
+    {
+        if (curr->rollnumber == toSearch)
+        {
+            cout << "Roll Number: " << curr->rollnumber << endl;
+            cout << "GPA: " << curr->gpa << endl;
+            break;
+        }
+        else
+        {
+            curr = curr->next;
+            if (curr == NULL) {
+                cout << "Element doesn't exist" << endl;
+                break;
+            }
+        }
+    }
 }
 
-int getLength(Node* head){
-	int len =0;
-	Node* temp = head;
-	
-	while(temp != NULL){
-		len++;
-		temp = temp->next;
-	}
-	return len;
+
+void print(studentRecord** head)
+{
+    studentRecord* curr = *head;
+    while (curr != NULL)
+    {
+        cout << "Roll Number: " << curr->rollnumber << endl;
+        cout << "GPA: " << curr->gpa << endl;
+        cout << "" << endl;
+        curr = curr->next;
+    }
 }
 
-void InsertAtPosition(Node* &tail, Node* &head, int position, int d){
- 	if(position==1){
- 		InsertAtHead(head,d);
- 		return;
-	 }
- 	Node* temp = head;
- 	int cnt = 1;
- 	while(cnt<position-1){
- 		temp = temp->next;
- 		cnt++; 
-	 }
-	 if(temp->next==NULL){
-	 	InsertAtTail(tail, d);
-	 	return;
-	 }
-	 Node* nodeToInsert = new Node(d);
-	 nodeToInsert-> next = temp-> next;
-	 temp-> next->prev = nodeToInsert;
-	 temp-> next = nodeToInsert;
-	 nodeToInsert->prev = temp;
-	 
- }
 
- void deleteNode(int position, Node* &head){
- 	if(position == 1){
- 		Node* temp = head; 
- 		temp->next->prev=NULL;
- 		head = temp -> next;
- 		temp->next = NULL;
- 		delete temp;	
-	 }
-	 else
-	 {
-	 	Node* curr = head;
-	 	Node* prev = NULL;
-	 	int cnt =1;
-	 	while(cnt<position) {
-	 		prev = curr;
-	 		curr = curr->next;
-	 		cnt++;
-		 }
-		 
-		 curr -> prev = NULL;
-		 prev -> next = curr -> next;
-		 curr -> next = NULL;
-		 delete curr;
-	 }
- }
+void del(studentRecord** head)
+{
+    int toDelete;
+    cout << "Insert Roll Number: ";
+    cin >> toDelete;
 
-int main(){
-	Node* node1= new Node(10);
-	Node* head = node1;
-	Node* tail = node1;
-	print (head);
-	//cout << getLength(head)<<endl;
-	
-	InsertAtHead(head, 12);
- 	
- 	print(head);
- 	
- 	InsertAtHead(head, 27 );
- 	
- 	print(head);
-	
-	InsertAtHead(head, 17);
- 	
- 	print(head);
- 	
- 	InsertAtTail(tail, 71);
- 	
- 	print(head);
- 	
- 	InsertAtPosition(tail, head, 2, 101);
- 	
- 	print(head);
-	
-	InsertAtPosition(tail, head, 4, 900);
- 	
- 	print(head);
-	
-	InsertAtPosition(tail, head, 5, 151);
- 	
- 	print(head);
-	
-	deleteNode(2, head);
-	
- 	print(head);
- 	
-	
-	return 0;
+
+    studentRecord* curr = *head;
+    while (curr != NULL)
+    {
+        if (curr->rollnumber == toDelete)
+        {
+            if (curr->prev != NULL)
+            {
+                curr->prev->next = curr->next;
+            }
+            if (curr->next != NULL)
+            {
+                curr->next->prev = curr->prev;
+            }
+            if (curr == *head)
+            {
+                *head = curr->next;
+            }
+            free(curr);
+            return;
+        }
+        curr = curr->next;
+    }
+    cout << "Doesn't Exist" << endl;
+}
+
+
+void delall(studentRecord** head)
+{
+    studentRecord* curr = *head;
+    while (curr != NULL)
+    {
+        studentRecord* temp = curr->next;
+        free(curr);
+        curr = temp;
+    }
+    *head = NULL;
+}
+
+void count(studentRecord** head)
+{
+    int count = 0;
+    studentRecord* curr = *head;
+    while (curr != NULL)
+    {
+        count++;
+        curr = curr->next;
+    }
+    cout << "Total number of elements in the list are " << count << endl;
+}
+
+int main()
+{
+    studentRecord* head = NULL;
+    cout << "List is currently empty" << endl;
+    insert(&head);
+    int select = 1;
+    cout << "Press 1 to insert" << endl;
+    cout << "Press 2 to search" << endl;
+    cout << "Press 3 to print" << endl;
+    cout << "Press 4 to delete" << endl;
+    cout << "Press 5 to delete all" << endl;
+    cout << "Press 6 to print the total number of elements" << endl;
+    cout << "Press 0 to exit" << endl;
+    cout << "Enter your choice: " << "";
+    cin >> select;
+    while (select != 0)
+    {
+        if (select == 1)
+        {
+            insert(&head);
+        }
+        if (select == 2)
+        {
+            search(&head);
+        }
+        if (select == 3)
+        {
+            print(&head);
+        }
+        if (select == 4)
+        {
+            del(&head);
+        }
+        if (select == 5)
+        {
+            delall(&head);
+        }
+        if (select == 6)
+        {
+            count(&head);
+        }
+
+        cout << "Enter your choice again: "
+            << "";
+        cin >> select;
+    }
 }
